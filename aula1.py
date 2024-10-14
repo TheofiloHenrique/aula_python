29332d0a5db2b90983983bf16fc255542206be26
Theofilo Henrique <151052072+TheofiloHenrique@users.noreply.github.com>
2024-10-14 20:34:59 -0300

    Init

---
+++ aula1.py
@@ -0,0 +1,59 @@
+# print("Hello World!")
+# #To comentando mano
+
+# #numeros
+# idade = 28
+# altura = 1.80
+
+# #booleans
+# verdadeiro = True
+# falso = False
+
+# #sequencia
+# lista = [123,True,"Guilherme",1.80,["outra coisa"]]
+# tupla = (123,True,"Guilherme",1.80)
+# dicionarios = {"nome": "Guilherme","idade":28}
+
+# valores_none = None
+
+# teste = 18
+# print(teste)
+# teste= "Jujuba"
+# print(teste)
+
+# #Tipo
+# print(type(teste))
+
+#entrada de dados
+#salario_base=1000
+#gratificacao = float(input("Digite o valor da gratificação: "))
+
+#processamento
+#salario_final = salario_base + gratificacao
+
+#saida
+#print("O salário final é de: ", salario_final)
+
+#print("O salário final é de: {}".format(salario_final))
+
+#print(f"O salário final é de {salario_final} e o cara ta liso kk")
+
+salario = float(input("Informe seu salário: "))
+
+emprestimo = float(input("Digite o valor do empréstimo: "))
+
+if (emprestimo <= salario * 0.50):
+    print("Empréstimo Aprovado!") 
+       
+elif (emprestimo <= salario * 0.75):
+    print("Empréstimo em Análise")
+    
+else:
+    print("Seu empréstimo não foi aprovado!")
+    
+    
+    
+    
+ 
+ 
+ 
